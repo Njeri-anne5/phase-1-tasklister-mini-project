c30b62fdf4fdb5c085611c12c4283995b8fdd5c2
+ <+>
2022-08-31 14:46:54 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form=document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    buildToDo(e.target.new_todo.value)
+    form.reset()//method form reset
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement("p")//creates p in the DOM
+  let button = document.createElemnt("button")//Creates button
+  button.addEventListener("click")//
+  button.textContent = "x"//text in the button
+  p.textContent = `${todo}  ` //
+  p.appendChild(button)
+  console.log("p")
+  document.querySelector("todo_container").appendChild(p)
+
+}
